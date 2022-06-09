bc4b63550d9d025f2524d09f69f5a79fa7012797
SebasVGC <2902340180301@ingenieria.usac.edu.gt>
2022-06-09 17:03:36 -0600

    clase 0609

---
+++ Clase 06-09/main.py
@@ -0,0 +1,15 @@
+from xml.dom import minidom
+doc = minidom.parse("items.xml")
+
+# nombre = doc.getElementsByTagName("nombre")[0]
+# print(nombre.firstChild.data)
+empleados = doc.getElementsByTagName("empleado")
+for empleado in empleados:
+    sid = empleado.getAttribute("id")
+    nombre = empleado.getElementsByTagName("nombre")[0]
+    username = empleado.getElementsByTagName("username")[0]
+    password = empleado.getElementsByTagName("password")[0]
+    print("id:%s " % sid)
+    print("username:%s" % username.firstChild.data)
+    print("password:%s" % password.firstChild.data)
+    print("------------------")
---
+++ Prueba/main.py
@@ -0,0 +1,2 @@
+lista = [1,11,2,3,5,0,13,4,7,10,3,7,9]
+print(lista[lista[lista[lista[8]]]])
